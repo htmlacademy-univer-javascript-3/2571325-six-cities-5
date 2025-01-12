5b9c931d17d46a5971e696ac54351ad9dab1002d
caskey99 <artem_6091@mail.ru>
2025-01-12 16:21:33 +0300

    replacing <a> tags with Link components

--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../constants/routers';
 
 interface TabProps {
   cityTitle: string;
@@ -8,12 +10,12 @@ interface TabProps {
 
 const Tab: React.FC<TabProps> = ({ cityTitle, isActiveCity, handleChangeActiveCity }) => (
   <li className="locations__item" data-testid="tab" onClick={handleChangeActiveCity}>
-    <a
+    <Link
       className={`locations__item-link tabs__item ${isActiveCity ? 'tabs__item--active' : ''}`}
-      href="#"
+      to={AppRoutes.Default}
     >
       <span>{cityTitle}</span>
-    </a>
+    </Link>
   </li>
 );
 
